5ca7cef6e4923fde4f8e3f1f92519ab84b8f2f73
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-12 09:08:54 -0700

    write js code to generate a random number from 0-2
    this will allow a random choice between 3 options

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+//Create function getComputerChoice that returns rock, paper, or scissors
+
+//Generate a random number from a set of 3
+//Based on resulting number, set variable to rock, paper, or scissors
+//return result
+
+let getComputerChoice = () => {
+    let x = Math.floor(Math.random() * 3);
+    console.log(x);
+}
+
+getComputerChoice();
