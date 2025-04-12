24b4b2e51da3dcf8de9393bdddd2fab5bf1818ef
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-12 10:45:25 -0700

    refactor code for readability

--- rps.js
+++ rps.js
@@ -17,91 +17,92 @@ let getHumanChoice = () => {
 
 let playGame = () => {
 
-let humanScore = 0;
-let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
 
-let playRound = (humanChoice, computerChoice) => {
-    switch(humanChoice) {
-        case 'rock':
-            switch(computerChoice) {
-                case 'scissors':
-                    humanScore++;
-                    console.log('You win! Rock beats Scissors.');
-                    break;
-                case 'paper':
-                    computerScore++;
-                    console.log('You lose! Paper beats Rock.');
-                    break;
-                case 'rock':
-                    console.log('Draw! Play Again!');
-                    break;
-            }
-            break;
-        case 'paper':
-            switch(computerChoice) {
-                case 'rock':
-                    humanScore++;
-                    console.log('You win! Paper beats Rock.');
-                    break;
-                case 'scissors':
-                    computerScore++;
-                    console.log('You lose! Scissors beats Paper.');
-                    break;
-                case 'paper':
-                    console.log('Draw! Play Again!');
-                    break;
-            }
-            break;
-        case 'scissors':
-            switch(computerChoice) {
-                case 'paper':
-                    humanScore++;
-                    console.log('You win! Scissors beats Paper.');
-                    break;
-                case 'rock':
-                    computerScore++;
-                    console.log('You lose! Rock beats Scissors.');
-                    break;
-                case 'scissors':
-                    console.log('Draw! Play Again!');
-                    break;
-            }
-            break;
+    let playRound = (humanChoice, computerChoice) => {
+        switch(humanChoice) {
+            case 'rock':
+                switch(computerChoice) {
+                    case 'scissors':
+                        humanScore++;
+                        console.log('You win! Rock beats Scissors.');
+                        break;
+                    case 'paper':
+                        computerScore++;
+                        console.log('You lose! Paper beats Rock.');
+                        break;
+                    case 'rock':
+                        console.log('Draw! Play Again!');
+                        break;
+                }
+                break;
+            case 'paper':
+                switch(computerChoice) {
+                    case 'rock':
+                        humanScore++;
+                        console.log('You win! Paper beats Rock.');
+                        break;
+                    case 'scissors':
+                        computerScore++;
+                        console.log('You lose! Scissors beats Paper.');
+                        break;
+                    case 'paper':
+                        console.log('Draw! Play Again!');
+                        break;
+                }
+                break;
+            case 'scissors':
+                switch(computerChoice) {
+                    case 'paper':
+                        humanScore++;
+                        console.log('You win! Scissors beats Paper.');
+                        break;
+                    case 'rock':
+                        computerScore++;
+                        console.log('You lose! Rock beats Scissors.');
+                        break;
+                    case 'scissors':
+                        console.log('Draw! Play Again!');
+                        break;
+                }
+                break;
+        }
     }
-}
 
-//Round 1
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+    //Round 1
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 
-//Round 2
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+    //Round 2
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 
-//Round 3
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+    //Round 3
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 
-//Round 4
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+    //Round 4
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 
-//Round 5
-humanSelection = getHumanChoice();
-computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+    //Round 5
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 
-if (humanScore > computerScore) {
-    console.log(`You win! ${humanScore} rounds to ${computerScore}`);
-} else if (computerScore > humanScore) {
-    console.log(`You lose! ${humanScore} rounds to ${computerScore}`);
-} else {
-    console.log(`Draw! ${humanScore} rounds to ${computerScore}`);
-}
+    //Final Scoring
+    if (humanScore > computerScore) {
+        console.log(`You win! ${humanScore} rounds to ${computerScore}`);
+    } else if (computerScore > humanScore) {
+        console.log(`You lose! ${humanScore} rounds to ${computerScore}`);
+    } else {
+        console.log(`Draw! ${humanScore} rounds to ${computerScore}`);
+    }
 }
 
 playGame();
